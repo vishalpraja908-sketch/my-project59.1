3487732e7359fd39c11e9ab7508d699b2544878c
vishalpraja908-sketch <vishalpraja908@gmail.com>
2025-12-12 20:31:03 +0530

    updated project code

--- src/Select.js
+++ src/Select.js
@@ -1,10 +1,13 @@
 
 
-function Select() {
+function Select({sort,handleSortChange}) {
   return (
     <div className="flex justify-end mb-4 mr-20">
-      <select className="border border-gray-300 rounded px-3 py-2 text-gray-700">
-        <option>Default sorting</option>
+      <select  onChange ={handleSortChange}className="border border-gray-300 rounded px-3 py-2 text-gray-700"
+        value={sort} >
+        <option value="default">Default sorting</option>
+        <option value="title">Sort by title</option>
+          <option value="price">Sort by price</option>
       </select>
     </div>
   );
